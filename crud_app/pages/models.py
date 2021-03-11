23d5101add21eb5bae178310614282126a3ca48f
ayse <ayses.aydogdu@gmail.com>
2021-03-11 22:23:58 +0100

    Employee Table is written in models file

--- crud_app/pages/models.py
+++ crud_app/pages/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Employee(models.Model):
+    name = models.CharField(max_length=200)
+    employee_ID = models.IntegerField()
+    team = models.CharField(max_length=100)
+    hourly_rate = models.FloatField()
