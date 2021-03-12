95dc84637d49a41b37f65f6d7548c39e20f53c2b
ayse <ayses.aydogdu@gmail.com>
2021-03-12 21:44:46 +0100

    detail and employee_list pages created

--- crud_app/pages/urls.py
+++ crud_app/pages/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('', views.index,name = "index"),
+#urlpatterns = [
+#    path('', views.index,name = "index"),
     # path(route, view, opt(shortcut name))
+#]
+
+urlpatterns = [
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # show all employees
+    path('employee_list', views.employee_list, name='employee_list'),
+    # ex: /polls/5/
+    path('<int:id>/', views.detail, name='detail'),
 ]
--- crud_app/pages/views.py
+++ crud_app/pages/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.http import HttpResponse
+
+
+from .models import Employee
+import pandas as pd
 
 def index(request):
     return render(request,'index.html')
 
+def employee_list(request):
+    # latest_question_list = Employee.objects.order_by('-id')
+    # names = [] 
+    # names.append([q.name for q in latest_question_list])
+    # rates = [] 
+    # rates.append([q.hourly_rate for q in latest_question_list])
+    # return HttpResponse(rates)
+    employee_list = Employee.objects.order_by('-id')[:5]
+    #template = loader.get_template('pages/employee_list.html')
+    context = {'employee_list': employee_list}
+    return render(request, 'pages/employee_list.html', context)
+
+def detail(request, id):
+    employee = get_object_or_404(Employee, pk=id)
+    return render(request, 'pages/detail.html', {'employee': employee})
 # Create your views here.
+# def detail(request, id):
+#     return HttpResponse("You're looking at employee %s." % id)
+
+# def results(request, id):
+#     response = "You're looking at the results of employee %s."
+#     return HttpResponse(response % id)
+
+# def vote(request, id):
+#     return HttpResponse("You're voting on question %s." % id)
