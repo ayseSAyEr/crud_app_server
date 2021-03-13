18996863afbcc5db2f21be928e137caf9a04ec8f
ayse <ayses.aydogdu@gmail.com>
2021-03-13 02:05:57 +0100

    create, update and delete paths are added in urls

--- crud_app/pages/urls.py
+++ crud_app/pages/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
 from . import views
 
-#urlpatterns = [
-#    path('', views.index,name = "index"),
-    # path(route, view, opt(shortcut name))
-#]
-
 urlpatterns = [
     # ex: /polls/
     path('', views.index, name='index'),
     # show all employees
     path('employee_list', views.employee_list, name='employee_list'),
-    # ex: /polls/5/
-    path('<int:id>/', views.detail, name='detail'),
+    # create 
+    path('create_employee', views.create_employee, name = 'create_employee'),
+    # update 
+    path('update_employee/<int:id>', views.update_employee, name = 'update_employee'),
+    # delete 
+    path('delete_employee/<int:id>', views.delete_employee, name = 'delete_employee'),
+    
+    # # ex: /polls/5/
+    # path('<int:id>/', views.detail, name='detail'),
+   
 ]
