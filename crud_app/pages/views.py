93b711802e038191e20c113eb524626998fe51e0
ayse <ayses.aydogdu@gmail.com>
2021-03-14 03:29:47 +0100

    comment added

--- crud_app/pages/views.py
+++ crud_app/pages/views.py
@@ -8,8 +8,10 @@ from .forms import EmployeeCreate
 def index(request):
     return render(request,'index.html')
 
+# create employee steps
 def create_employee(request):
     upload = EmployeeCreate()
+    # when the post coming, it creates new employee
     if request.method == 'POST':
         upload = EmployeeCreate(request.POST, request.FILES)
         if upload.is_valid():
@@ -20,19 +22,22 @@ def create_employee(request):
     else:
         return render(request, 'pages/upload_form.html', {'upload_form':upload})
 
+# delete employee steps
 def delete_employee(request, id):
+    # delete the relevant employee according to the selected id
     id = int(id)
     try:
-        employee_sel = Employee.objects.get(employee_ID = id)
+        employee_sel = Employee.objects.get(id = id)
     except Employee.DoesNotExist:
         return redirect('index')
     employee_sel.delete()
     return redirect('employee_list')
 
+# update employee steps
 def update_employee(request, id):
     id = int(id)
     try:
-        employee_sel = Employee.objects.get(employee_ID = id)
+        employee_sel = Employee.objects.get(id = id)
     except Employee.DoesNotExist:
         return redirect('index')
     employee_form = EmployeeCreate(request.POST or None, instance = employee_sel)
@@ -41,6 +46,7 @@ def update_employee(request, id):
        return redirect('employee_list')
     return render(request, 'pages/upload_form.html', {'upload_form':employee_form})
 
+# shows all employees in the employee list
 def employee_list(request):
     employee_list = Employee.objects.order_by('-id')[:5]
     context = {'employee_list': employee_list}
