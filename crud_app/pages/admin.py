647cd23bfa2930a8ef14b2dad586d3937c47d80c
ayse <ayses.aydogdu@gmail.com>
2021-03-14 03:27:42 +0100

    created new tables and for showing this page,  admin page is updated

--- crud_app/pages/admin.py
+++ crud_app/pages/admin.py
@@ -1,4 +1,22 @@
 from django.contrib import admin
 from . models import Employee
+from . models import Team
+from . models import TeamEmployee
 
+# for selecting foreignkey, TeamEmployeeAdmin class is generated 
+class TeamEmployeeAdmin(admin.ModelAdmin):
+    fields = ('employee_id', 'team_id', 'is_lead')
+
+    # this selects and displays id value of Employee class in admin add page
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(TeamEmployeeAdmin, self).get_form(request, obj, **kwargs)
+        form.base_fields['employee_id'].label_from_instance = lambda obj: "{} {}".format(obj.name, obj.surname)
+        form.base_fields['team_id'].label_from_instance = lambda obj: "{}".format(obj.team_name)
+        # form.base_fields['employee_id'].label_from_instance = lambda obj: "{} {} {}".format(obj.id, obj.name, obj.surname)
+        return form
+
+
+# add admin page models registration
 admin.site.register(Employee)
+admin.site.register(Team)
+admin.site.register(TeamEmployee,TeamEmployeeAdmin)
